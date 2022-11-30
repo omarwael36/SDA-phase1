67d81f23ff91a3d2bed20bc1aa1dfd159af5b890
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2022-11-30 23:32:55 +0200

    user class update

--- sourcecode/sourcecode/user.java
+++ sourcecode/sourcecode/user.java
@@ -1,49 +1,54 @@
 package sourcecode;
+import java.io.File;  
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 public class user {
 	private String email;
 	private String password;
 	private String username;
 	private static int walletID;
-	
-	public user(){
-		
+	public user(String e,String pw,String un){
+		this.email=e;
+		this.password=pw;
+		this.username = un;
 	}
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	public String getUsername() {
 		return username;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	
 
 	public int getWalletID() {
 		return walletID;
 	}
 
-	public void setWalletID(int walletID) {
-		this.walletID = walletID;
-	}
-	public void signup(String e,String pw, String un) {
-		this.setEmail(e);
-		this.setPassword(pw);
-		this.setUsername(un);
+	
+	public void signup() throws IOException {
+		FileWriter obj = new FileWriter("usersInfo",true);
+		Scanner scan = new Scanner(new File("usersInfo"));
+		boolean flag = true;
+        while(scan.hasNext()){
+            String line = scan.nextLine().toLowerCase().toString();
+            if(line.contains(this.getEmail()) || line.contains(this.getUsername())){
+            	flag=false;
+                System.out.println("the username or email that you enterd is taken!");
+            }
+        }
+        scan.close();
+        if(flag) {
+        	obj.appendInfile
+        }
+		
 	}
 	public boolean login (String un,String pw) {
 		if(this.getUsername()==un && this.getPassword()==pw ) {
