245068435334a48d8bb86f2774427369d12b4c54
YLabatia <119506190+YLabatia@users.noreply.github.com>
2022-11-30 15:10:44 +0200

    Update main.java

--- sourcecode/sourcecode/main.java
+++ sourcecode/sourcecode/main.java
@@ -2,6 +2,6 @@ package sourcecode;
 
 public class main {
 	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Messi weino kassarna 3eino!!!"); 
     }
 }
