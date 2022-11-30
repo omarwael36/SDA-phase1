554a9028e0709dd2002a0c2d1f1071e869fa6ff7
YLabatia <119506190+YLabatia@users.noreply.github.com>
2022-11-30 15:11:36 +0200

    Update main.java

--- sourcecode/sourcecode/main.java
+++ sourcecode/sourcecode/main.java
@@ -3,5 +3,6 @@ package sourcecode;
 public class main {
 	public static void main(String[] args) {
         System.out.println("Messi weino kassarna 3eino!!!"); 
+		System.out.println("Ankara messi ankara messi goal goal goal !!!");
     }
 }
