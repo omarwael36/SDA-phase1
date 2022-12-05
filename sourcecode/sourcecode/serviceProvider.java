58f68c8cdc9717d27ef93aeae68a3461f32bf5e4
Omar Waleed Zenhom <93471291+OmarZen@users.noreply.github.com>
2022-12-05 19:08:39 +0200

    Update serviceProvider.java

--- sourcecode/sourcecode/serviceProvider.java
+++ sourcecode/sourcecode/serviceProvider.java
@@ -1,5 +1,18 @@
 package sourcecode;
 
-public abstract class serviceProvider {
-	abstract public services createService(String s);
+public abstract class serviceProvider{
+    private String name;
+    private float taxes;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getTaxes() {
+        return taxes;
+    }
+    public void setTaxes(float taxes) {
+        this.taxes = taxes;
+    }
 }
