28cf81a1be83cef9bcff7d716bf8e82e2741b776
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2022-12-05 23:58:25 +0200

    cash on delivery

--- sourcecode/sourcecode/cash_on_delivery.java
+++ sourcecode/sourcecode/cash_on_delivery.java
@@ -1,5 +1,13 @@
 package sourcecode;
 
 public class cash_on_delivery {
+	private String address;
 
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
