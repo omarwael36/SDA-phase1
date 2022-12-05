2cde5972ed1636fc80a796e109f09fb65893f50a
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2022-12-05 23:53:51 +0200

    credit card

--- sourcecode/sourcecode/credit.java
+++ sourcecode/sourcecode/credit.java
@@ -1,5 +1,41 @@
 package sourcecode;
 
-public class credit {
-
+public class credit extends payment {
+	private double amount;
+	private String expiredate;
+	private String securitydate;
+	private String cardnumber;
+	
+	public credit() {
+		
+	}
+	public double pay(double c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getExpiredate() {
+		return expiredate;
+	}
+	public void setExpiredate(String expiredate) {
+		this.expiredate = expiredate;
+	}
+	public String getSecuritydate() {
+		return securitydate;
+	}
+	public void setSecuritydate(String securitydate) {
+		this.securitydate = securitydate;
+	}
+	public String getCardnumber() {
+		return cardnumber;
+	}
+	public void setCardnumber(String cardnumber) {
+		this.cardnumber = cardnumber;
+	}
+	
 }
