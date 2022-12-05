b4f79c693848ed76694a8e03e6bfc5d707e74d8e
YLabatia <119506190+YLabatia@users.noreply.github.com>
2022-12-05 22:21:43 +0200

    Update donations.java

--- sourcecode/sourcecode/donations.java
+++ sourcecode/sourcecode/donations.java
@@ -1,5 +1,17 @@
 package sourcecode;
 
-public class donations {
-
+abstract public class donations extends services{
+	public donations(){
+		name.add("internet payment");
+		serviceProvider temp = new serviceProvider("school",0);
+		sp.add(temp);
+		temp = new serviceProvider("NGO", 0);
+		sp.add(temp);
+		temp = new serviceProvider("cancer hospital", 0);
+		sp.add(temp);
+	}
+	public double cost(int n, int i){
+		return n +( n*sp.get(i).getTaxes());
+	}
 }
+
