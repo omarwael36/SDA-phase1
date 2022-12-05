cb16df78c9c1319b3c887421491bc9f68b333d17
Omar Waleed Zenhom <93471291+OmarZen@users.noreply.github.com>
2022-12-05 22:34:44 +0200

    Update overallDiscount.java

--- sourcecode/sourcecode/overallDiscount.java
+++ sourcecode/sourcecode/overallDiscount.java
@@ -1,5 +1,25 @@
 package sourcecode;
 
-public class overallDiscount {
+public class overallDiscount extends discount {
+	private services ser;
+	public overallDiscount() {
+		dis.add("overall discount");
+	}
+	public overallDiscount(String n) {
+		dis.add(n);
+	}
+	public double cost(int n,int i) {
+		
+		return getSer().cost(n, i) - (n*this.getRate()/100);
+	}
+	public services getSer() {
+		return ser;
+	}
+	public void setSer(services ser) {
+		this.ser = ser;
+	}
+
+
+	
 
 }
